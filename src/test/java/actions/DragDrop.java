f9a3e2abe3658193bdc37c08e0cacab1e564d38a
naveenreddykotha <naveenreddykotha7@gmail.com>
2024-01-29 21:40:45 +0530

    webdrivermanager added in dragdrop class

--- src/test/java/actions/DragDrop.java
+++ src/test/java/actions/DragDrop.java
@@ -8,11 +8,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DragDrop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "D:\\Download\\chromedriver_win32\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "D:\\Download\\chromedriver_win32\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.get("https://demoqa.com/menu/#");
