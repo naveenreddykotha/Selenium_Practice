b5a596a42ca0bfa0c42942126220384f2d5c8634
naveenreddykotha <naveenreddykotha7@gmail.com>
2024-01-29 19:46:10 +0530

    In Alerts.java class added webdrivermanager setup

--- src/test/java/dropdowns/Alerts.java
+++ src/test/java/dropdowns/Alerts.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Alerts {
 
 	public static void main(String[] args) {
@@ -14,7 +16,8 @@ public class Alerts {
 		//Selinium by default cannot handle this
 		//this provide one mechanism to manage alerts
 		String text="naveen";
-		System.setProperty("webdriver.chrome.driver", "D:\\Download\\chromedriver_win32\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "D:\\Download\\chromedriver_win32\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
